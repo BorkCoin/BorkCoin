cac0427cbf78cc5444506a1abc196c3096457add
Alexander Leitner <leitnersalex@gmail.com>
2018-02-17 15:16:35 -0700

    Restructured

--- contracts/BorkCoin.sol
+++ contracts/BorkCoin.sol
@@ -6,30 +6,101 @@ import './Ownable.sol';
 contract Bork {
   using SafeMath for uint256;
   string public name;
+  string public data_type;
   uint256 public totalSupply;
   int[] private data;
   address public creator;
+  uint256 public price;
+  address[] private approvalPool;
+  address[] private declinePool;
+  enum State { Pending, Approved, Rejected, Published }
+  uint private state;
+  address[] public committee;
 
-  mapping(address => uint256) balances;
+  mapping(address => uint256) public balances;
 
-  function Bork(address _creator, string _name, uint256 _totalSupply, int[] _data) public {
+  function Bork(address _creator, address[] _committee, uint256 _price, string _type, string _name, uint256 _totalSupply, int[] _data) public {
     name = _name;
     totalSupply = _totalSupply;
     data = _data;
     creator = _creator;
-    balances[_creator] = totalSupply;
+    data_type = _type;
+    price = _price;
+    state = uint(State.Pending);
+    committee = _committee;
+
+    if (_totalSupply < 5) revert(); // One for the approvers, one for the creator
   }
 
   function balanceOf(address _owner) external view returns (uint256 balance) {
       return balances[_owner];
   }
 
-  function transfer(address _from, address _to, uint256 _value) external {
+  modifier onlyCommittee() {
+    bool exists = false;
+    for(uint i = 0; i < committee.length; i++) {
+      if(msg.sender == committee[i]) exists = true;
+    }
+
+    require(exists);
+    _;
+  }
+
+  function transfer(address _to, uint256 _value) external {
     if (_to == 0x0) revert();
-    if (balances[_from] < _value) revert();
+    if (balances[msg.sender] < _value) revert();
     if (balances[_to].add(_value) < balances[_to]) revert();
     balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+  }
+
+  function buy() external payable {
+    if (msg.sender == 0x0) revert();
+    if (msg.value != price) revert();
+    if (balances[msg.sender].add(1) > totalSupply) revert();
+
+    balances[msg.sender] = balances[msg.sender].add(1);
+  }
+
+  function approve(address _approver) onlyCommittee external {
+    if (hasAlreadyVoted(_approver, approvalPool)) revert();
+    if (state != uint(State.Pending)) revert();
+    approvalPool.push(_approver);
+
+    if (approvalPool.length > committee.length/2) {
+      state = uint(State.Approved);
+    }
+  }
+
+  function decline(address _decliner) onlyCommittee external {
+    if (hasAlreadyVoted(_decliner, declinePool)) revert();
+    if (state != uint(State.Pending)) revert();
+
+    declinePool.push(_decliner);
+
+    if (declinePool.length > committee.length/2) {
+      state = uint(State.Rejected);
+    }
+  }
+
+  function hasAlreadyVoted(address _voter, address[] arr) private pure returns (bool) {
+    for(uint i = 0; i < arr.length; i++) {
+      if(_voter == arr[i]) return true;
+    }
+
+    return false;
+  }
+
+  function publish() public {
+    if (creator != msg.sender) revert();
+    if (state != uint(State.Approved)) revert();
+
+    for(uint i = 0; i < approvalPool.length; i++) {
+      if(approvalPool[i] == creator) balances[approvalPool[i]] = balances[approvalPool[i]].add(1);
+    }
+
+    balances[creator] = balances[creator].add(1);
+    state = uint(State.Published);
   }
 
 }
@@ -41,17 +112,20 @@ contract BorkCoin is Ownable {
   string public symbol = "BKC";
   uint256 public decimals = 0;
   address[] private borks;
-  address[] private pendingBorks;
-  mapping(address => address[]) public approvalPool;
-  mapping(address => address[]) public declinePool;
 
   uint private maximumBorks = 20;
-  mapping(address => uint256) public balances;
   address[] private eliteBorkers;
   uint private maximumEliteBorkers = 5;
 
   function BorkCoin() public {
     eliteBorkers.push(msg.sender);
+  }
+
+  function balanceOf(address _owner) external view returns (uint256 balance) {
+      balance = 0;
+      for(uint i = 0; i < borks.length; i++) {
+        balance = balance.add(Bork(borks[i]).balanceOf(_owner));
+      }
   }
 
   modifier onlyEliteBorker() {
@@ -66,7 +140,6 @@ contract BorkCoin is Ownable {
 
   function addEliteBorker(address _newGuy) public onlyOwner {
     if (eliteBorkers.length >= maximumEliteBorkers) revert();
-
     eliteBorkers.push(_newGuy);
   }
 
@@ -76,14 +149,6 @@ contract BorkCoin is Ownable {
     }
 
     return eliteBorkers.length; // Returns a number greater than max index
-  }
-
-  function hasEliteBorkerVoted(address _eliteBorker, address[] arr) private pure returns (bool) {
-    for(uint i = 0; i<arr.length; i++) {
-      if(_eliteBorker == arr[i]) return true;
-    }
-
-    return false;
   }
 
   function removeEliteBorker(address _loser) public onlyOwner {
@@ -97,72 +162,11 @@ contract BorkCoin is Ownable {
     eliteBorkers.length--;
   }
 
-  function createBork(string _name, uint256 _totalSupply, int[] _data) public onlyEliteBorker {
+  function createBork(string _name, uint256 _price, string _type, uint256 _totalSupply, int[] _data) public onlyEliteBorker {
     // TODO: Check if name already exists???
     if (borks.length > maximumBorks) revert();
-    address newBork = new Bork(msg.sender, _name, _totalSupply, _data);
-    pendingBorks.push(newBork);
-  }
-
-  function getPendingBorkIndex(address _pendingBork) private view returns (uint) {
-    for(uint i = 0; i<pendingBorks.length; i++) {
-      if(_pendingBork == pendingBorks[i]) return i; // Returns index
-    }
-
-    return pendingBorks.length; // Returns a number greater than max index
-  }
-
-  function approveBork(uint index) public onlyEliteBorker {
-    if (borks.length > maximumBorks) revert();
-    if (hasEliteBorkerVoted(msg.sender, approvalPool[pendingBorks[index]])) revert();
-    approvalPool[pendingBorks[index]].push(msg.sender);
-
-    if (approvalPool[pendingBorks[index]].length >= eliteBorkers.length/2) {
-      borks.push(pendingBorks[index]);
-      Bork bork = Bork(borks[borks.length - 1]);
-      balances[bork.creator()] = balances[bork.creator()].add(bork.totalSupply());
-      // Clear out the old pending stuff
-      deletePendingBork(index);
-    }
-
-  }
-
-  function declineBork(uint index) public onlyEliteBorker {
-    if (pendingBorks.length <= 0) revert();
-    if (hasEliteBorkerVoted(msg.sender, declinePool[pendingBorks[index]])) revert();
-    declinePool[pendingBorks[index]].push(msg.sender);
-
-    if (declinePool[pendingBorks[index]].length >= eliteBorkers.length/2) {
-      // Clear out the old pending stuff
-      deletePendingBork(index);
-    }
-  }
-
-  function deletePendingBork(uint index) private {
-    if (pendingBorks.length <= 0) revert();
-
-    pendingBorks[index] = pendingBorks[pendingBorks.length-1];
-    delete pendingBorks[pendingBorks.length - 1];
-    pendingBorks.length--;
-
-    for(uint i = 0; i<declinePool[pendingBorks[index]].length; i++) {
-      delete declinePool[pendingBorks[index]][i];
-    }
-    for(uint j = 0; j<approvalPool[pendingBorks[index]].length; j++) {
-      delete approvalPool[pendingBorks[index]][j];
-    }
-  }
-
-  function transfer(address _bork, address _to, uint256 _value) public {
-    if (_to == 0x0) revert();
-    if (balances[msg.sender] < _value) revert();
-    if (balances[_to].add(_value) < balances[_to]) revert();
-
-    Bork(_bork).transfer(msg.sender, _to, _value);
-
-    // Maybe we can get rid of the below and just loop through everything instead to get balance.
-    balances[_to] = balances[_to].add(_value);
-    balances[msg.sender] = balances[msg.sender].sub(_value);
+    address newBork = new Bork(msg.sender, eliteBorkers, _price, _type, _name, _totalSupply, _data);
+    borks.push(newBork);
   }
 
 }
